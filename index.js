e19a3d59160ad7be6fbcaaa4a2a0d2836b2c6846
Agarwal, Ved Prakash <vedagarw@visa.com>
2017-03-15 13:35:26 +0530

    Minor Fixes; Adding Logger

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var rp = require('request-promise');
 var BPromise = require('bluebird');
 var schedule = require('node-schedule');
 
-
-module.exports = function (services, intervalInMin) {
-    services = services || {};
+module.exports = function checkHealth(configuration, intervalInMin, logger) {
+    var services = configuration || [];
 
     var healthStatus = {
         status: 'AVAILABLE',
@@ -15,17 +14,12 @@ module.exports = function (services, intervalInMin) {
     };
 
     var rule = new schedule.RecurrenceRule();
-
     rule.minute = new schedule.Range(0, 59, intervalInMin);
 
-    schedule.scheduleJob(rule, function () {
-
-        console.log("Probing HealthCheck For Services");
-
+    var job = schedule.scheduleJob(rule, function () {
         BPromise.map(services, function (service) {
-
                 if (service.type === 'REST') {
-                    //Set Request Options
+                    // Set Request Options
                     var options = {
                         rejectUnauthorized: false,
                         simple: true,
@@ -45,35 +39,27 @@ module.exports = function (services, intervalInMin) {
                         healthStatus.status = 'UNAVAILABLE';
                         err.response.body.serviceName = service.serviceName;
                         return err.response.body;
-                    })
-                }
-                else if (service.type === 'MONGO') {
-
+                    });
+                } else if (service.type === 'MONGO') {
                     return new BPromise(function (resolve) {
-
                         var resp = {
                             timestamp: new Date(),
                             serviceName: service.serviceName
-                        }
+                        };
 
                         service.connection.db.collection(service.testCollectionName, function (err, collection) {
-                            collection.findOne({}, function (err, data) {
-                                if (data) {
-                                    resp.status = 'AVAILABLE'
+                            collection.findOne({}, function (error) {
+                                if (error) {
+                                    resp.status = 'UNAVAILABLE';
                                 } else {
-                                    resp.status = 'UNAVAILABLE'
+                                    resp.status = 'AVAILABLE';
                                 }
                                 resolve(resp);
                             });
                         });
-
-
                     });
-
-                } else {
-                    throw new Error("Type Not Supported");
                 }
-
+                throw new Error('Service Type Not Supported');
             }
         ).then(function (results) {
             healthStatus.services = [];
@@ -85,28 +71,34 @@ module.exports = function (services, intervalInMin) {
                         timestamp: result.timestamp
                     }
                 });
-            })
+            });
         }).catch(function (err) {
-            console.log(err);
+            if (logger) {
+                logger.err(err);
+            } else {
+                console.err(err);
+            }
         });
-
     });
 
-    return function (req, res, next) {
+    job.on('run', function () {
+        if (logger) {
+            logger.info('Overall Component Health Status : ' + healthStatus.status);
+        } else {
+            console.log('Overall Component Health Status : ' + healthStatus.status);
+        }
+    });
 
-        if (healthStatus.status == "AVAILABLE") {
-            healthStatus.message = "All the components are up and running - Uptime : " + process.uptime() + " sec";
+
+    return function (req, res) {
+        if (healthStatus.status === 'AVAILABLE') {
+            healthStatus.message = 'All the components are up and running - Uptime : ' + process.uptime() + ' sec';
             res.status(200).json(healthStatus);
         } else {
-            healthStatus.message = "One or more component(s) not available";
+            healthStatus.message = 'One or more component(s) not available';
             res.status(503).json(healthStatus);
         }
-
-    }
-
-}
-
-
-
+    };
+};
 
 
